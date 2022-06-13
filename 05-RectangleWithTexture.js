e3a7b055470fac5f35e9eec3c1ffe5ecab80401c
Mauricio Meza <mmezab@unal.edu.co>
2022-06-12 23:01:58 -0500

    texturas semi arreglado

--- 05-RectangleWithTexture.js
+++ 05-RectangleWithTexture.js
@@ -3,13 +3,13 @@
 //-------------------------------------
 vert_shader = glsl`
     //receive data from Buffer into gl_position (pos of current vertex)
-    attribute vec2 a_position;
+    attribute vec4 a_position;
     attribute vec2 a_texture;
     varying vec2 v_texture;
 
     void main() {
         v_texture = a_texture;
-        gl_Position = vec4(a_position, 1.0, 1.0);    
+        gl_Position = a_position;    
     }
 `;
 
@@ -31,23 +31,31 @@ precision mediump float;
 //-------------------------------------
 //----------WebGL_JS_Code-------------
 //-------------------------------------
-main();
-
-function main(){
-    var img = new Image();
-    img.onload = function(){
-        render(img);
-    }
-    img.src = "./cuy.jpg";
-}
+render();
 
 //Main Function
-function render(img){
+function render(){
     //Get canvas, context and programs
     var canvas = document.getElementById("canvas_05");
     var gl = canvas.getContext("webgl");
     var program = createProgramFromShaders(gl, vert_shader, frag_shader);
     gl.useProgram(program);
+
+     //Create texture buffer
+     var textureAttributeLocation = gl.getAttribLocation(program, "a_texture");
+     var textureBuffer = gl.createBuffer();
+     gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+     var uv = [
+         0,0,
+         1,0,
+         1,1,
+         0,0,
+         0,1,
+         1,1
+     ];
+     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.STATIC_DRAW);
+     gl.enableVertexAttribArray(textureAttributeLocation)
+     gl.vertexAttribPointer(textureAttributeLocation, 2, gl.FLOAT, false, 0, 0)
 
     //Create square buffer
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
@@ -65,31 +73,20 @@ function render(img){
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    //Create texture buffer
-    var textureAttributeLocation = gl.getAttribLocation(program, "a_texture");
-    var textureBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-    createRectangle(gl, 0, 0, 1, 1)
-    gl.enableVertexAttribArray(textureAttributeLocation)
-    gl.vertexAttribPointer(textureAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-
+    var img = new Image();
+    img.src = "./cuy.jpg";
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
-
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);     
     
     //Deifine Screen
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0,0,0,0)
     gl.clear(gl.COLOR_BUFFER_BIT);
-
-    //Draw
-    gl.drawArrays(gl.TRIANGLES, 0, 6)
 
     //Draw
     gl.drawArrays(gl.TRIANGLES, 0, 6)
--- 15-3DTextureAnimated.js
+++ 15-3DTextureAnimated.js
@@ -80,7 +80,6 @@ function main(){
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-        //Pre-generate different size textures
     });
 
     //Define Perspective Matrix
