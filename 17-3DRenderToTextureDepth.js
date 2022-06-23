5193eee7f2ab1bfea343acac2cf42e7cb8e95bb9
Mauricio Meza <mmezab@unal.edu.co>
2022-06-23 00:44:57 -0500

    render buffer

--- 17-3DRenderToTextureDepth.js
+++ 17-3DRenderToTextureDepth.js
@@ -2,7 +2,6 @@
 //----------VertexShader---------------
 //-------------------------------------
 vert_shader = glsl`
-    //receive data from Buffer into gl_position (pos of current vertex)
     attribute vec4 a_position;
     attribute vec2 a_tex_coord;
     uniform mat4 u_matrix_perspective;
@@ -21,9 +20,7 @@ vert_shader = glsl`
 //----------FragmentShader-------------
 //-------------------------------------
 frag_shader = glsl`
-
-precision mediump float;
-     //Turn pixel into the uniform color
+    precision mediump float;
     varying vec2 v_tex_coord;
 
     uniform sampler2D u_texture;
@@ -36,21 +33,17 @@ precision mediump float;
 //-------------------------------------
 //----------WebGL_JS_Code-------------
 //-------------------------------------
-
 main();
-
-
 //Main Function
 function main(){
-    //-------GET CANVAS & GL_CONTEXT-------
+    //Cargar el canvas y los shaders
     var canvas = document.getElementById("canvas_16");
     var gl = canvas.getContext("webgl");
     var program = createProgramFromShaders(gl, vert_shader, frag_shader);
     gl.useProgram(program);
 
 
-    //-----CREATE UVs & VERTEX BUFFERS-----
-    //--uv buffer
+    //Crear Buffer de Coordenadas de Textura (Coordenadas UV)
     var colorAttributeLocation = gl.getAttribLocation(program, "a_tex_coord")
     var colorBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
@@ -59,7 +52,7 @@ function main(){
     gl.enableVertexAttribArray(colorAttributeLocation);
     gl.vertexAttribPointer(colorAttributeLocation, 2, gl.FLOAT, false, 0, 0)
     
-    //--vetex buffer
+    //Crear Buffer de Geometria
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
     var positionBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -74,8 +67,8 @@ function main(){
     gl.uniformMatrix4fv(viewUniformLocation, false, viewMatrix);
 
 
-    //----------DEFINE TEXTURES----------
-    //--cube cuy texture
+    //----------DEFINIR TEXTURAS----------
+    //Crear Textura de Cuy
     var cuyTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, cuyTexture);
     var image = new Image();
@@ -90,26 +83,22 @@ function main(){
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
     })
 
+    //Crear Textura donde se renderizara la escena
     var targetTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 256, 256, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 256, 256, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
 
-    //-------DEFINE FRAME BUFFERS-------
+
+    //-------DEFINIR FRAME y RENDER BUFFERS-------
     //frame buffer
     var frameBuffer = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, 0)
-    
-    //depth buffer
-    var depthBuffer = gl.createRenderbuffer();
-    gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
-    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, 256, 256);
-    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
-      
+          
     
     //-------- DEFINE SCREEN --------
     gl.enable(gl.DEPTH_TEST) 
@@ -119,30 +108,32 @@ function main(){
     //-------- DRAW --------
     var Ry = 0;
     requestAnimationFrame(anim);
-    //Draw
+
     function anim(){
         Ry += 0.01;
-
+        //Render a Frame Buffer con textura de imagen y fondo azul
         gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
         gl.bindTexture(gl.TEXTURE_2D, cuyTexture);
         gl.viewport(0, 0, 256, 256);
         gl.clearColor(0,0,1,1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        drawCube(256/256);
+        drawCube();
 
+        //Render a Canvas con textura de buffer y fondo azul
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
         gl.bindTexture(gl.TEXTURE_2D, targetTexture);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         gl.clearColor(1,1,1,1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        drawCube(0, gl.canvas.width/gl.canvas.height);
+        drawCube();
         
         requestAnimationFrame(anim);
     }   
     
-    function drawCube(aspect){
+    function drawCube(){
         //Define Perspective Matrix
         for(var i=0; i<3; i++){
+            aspect = gl.canvas.width/gl.canvas.height
             const perspectiveMatrix = getMatrix3DPerspective(0.01, 100, 0.85, aspect);
             var perspectiveUniformLocation = gl.getUniformLocation(program, "u_matrix_perspective")
             gl.uniformMatrix4fv(perspectiveUniformLocation, false, perspectiveMatrix);
